697a72b23d366ee77fd00b15b7837d72aa18eee2
Kiriru Trofimov <kiriru.trofimov@gmail.com>
2023-04-01 11:41:17 +0200

    docker2squash.sh: adding compression level https://techoverflow.net/2013/01/11/creating-a-highly-compressed-squashfs-from-a-folder/

--- docker2squash.sh
+++ docker2squash.sh
@@ -15,4 +15,4 @@ trap cleanup EXIT
 CONTAINER=$(docker create "$1")
 docker export "$CONTAINER" | tar -C "$TMPDIR" -p -s --same-owner -xv
 docker rm "$CONTAINER"
-mksquashfs "$TMPDIR" "$OUTFILE" -info -processors 1 -noappend -comp xz -Xdict-size 100%
+mksquashfs "$TMPDIR" "$OUTFILE" -info -noappend -b 1048576 -comp xz -Xdict-size 100%
